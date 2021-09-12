9104a02161674d35e2a0b6c3fa922378986a6ee5
Apu Debnath <apu1578@gmail.com>
2021-09-13 05:40:59 +0600

    update content

--- js/app.js
+++ js/app.js
@@ -19,9 +19,10 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <p><span>Average Rating: ${product.rating.rate}</span> | <span>Count:  ${product.rating.count}</span></p>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn cart-btn">add to cart</button>
+      <button id="details-btn" class="btn details-color">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
