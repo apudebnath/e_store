46ace29378824083db97fad1e5951763776b18f8
Apu Debnath <apu1578@gmail.com>
2021-09-13 20:14:29 +0600

    update content

--- js/app.js
+++ js/app.js
@@ -19,15 +19,42 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
-      <p><span>Average Rating: ${product.rating.rate}</span> | <span>Count:  ${product.rating.count}</span></p>
+      <p><span class="textColor">Average <i class="fas fa-star"></i> ${product.rating.rate}</span> | <span class="countColor"><i class="fas fa-users"></i> ${product.rating.count}</span></p>
+        <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn cart-btn">add to cart</button>
-      <button id="details-btn" class="btn details-color">Details</button></div>
+      <button onclick="addDetails(${product.id})" id="details-btn" class="btn details-color">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
 };
-//${product.rating.count} ${product.rating.rate}
+
+// show single Product Details
+const addDetails = (id) => {
+    const url = `https://fakestoreapi.com/products/${id}`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => showProductDetails(data))
+
+  const showProductDetails = (product) => {
+    console.log(product);
+    const productDetails = document.getElementById('productDetails');
+    productDetails.innerHTML = `
+      <div class='single-productDetails w-50 mx-auto p-5 mb-5 d-flex justify-content-between text-white'>
+      <div class="align-middle">
+        <img class="product-image" src="${product.image}">
+      </div>
+      <div class="ps-3">
+        <h3 class="mt-0">${product.title}</h3>
+        <p><span class="text-info">Average <i class="fas fa-star"></i> ${product.rating.rate}</span> | <span class="text-warning"><i class="fas fa-users"></i> ${product.rating.count}</span></p>
+        <p>Category: ${product.category}</p>
+        <p><span class="fw-bold">Product Details: </span>${product.description}</p>
+      </div>
+      </div>
+    `;
+  }
+};
+// cart
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
